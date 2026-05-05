a4148ddede268ff70942d855f0c5449fba7cf2f8
juancontrerasp <juan.contreras-p@mail.escuelaing.edu.co>
2026-05-04 20:32:58 -0500

    Feat: starting position update

--- src/main/java/com/aguardientes/azarcafetero/parques_service/application/usecases/CreateGameUseCase.java
+++ src/main/java/com/aguardientes/azarcafetero/parques_service/application/usecases/CreateGameUseCase.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class CreateGameUseCase {
 
     private static final int[] EXIT_POSITIONS = {0, 17, 32, 49};
-    private static final String[] COLORS = {"AMARILLO", "AZUL", "VERDE", "ROJO"};
+    private static final String[] COLORS = {"AMARILLO", "ROJO", "VERDE", "AZUL"};
 
     private final GameRepository repository;
 
